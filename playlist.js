e1025ffeee61ea1e7e15eb520085b3a4276cfa65
Aliaksei Rudak <lexarudak@gmail.com>
2022-08-16 17:11:00 +0600

    fix: playlist url

--- playlist.js
+++ playlist.js
@@ -1,39 +1,39 @@
-// playlist for momentum_extantion app
+// playlist for mmomentum_for_chrome app
 
 const musicList = [
    {
     title: 'Валентин Стрыкало - Кладбище самолетов',
-    src: '../momentum_extantion/music/Валентин Стрыкало - Кладбище самолетов.mp3',
+    src: '../mmomentum_for_chrome/music/Валентин Стрыкало - Кладбище самолетов.mp3',
     duration: '05:52'
   },
    {
     title: 'Операция Пластилин - Соя',
-    src: '../momentum_extantion/music/Операция Пластилин - Соя.mp3',
+    src: '../mmomentum_for_chrome/music/Операция Пластилин - Соя.mp3',
     duration: '02:47'
   },
    {
     title: 'Anacondaz - Пусть они умрут (feat. Noize MC)',
-    src: '../momentum_extantion/music/Anacondaz - Пусть они умрут (feat. Noize MC).mp3',
+    src: '../mmomentum_for_chrome/music/Anacondaz - Пусть они умрут (feat. Noize MC).mp3',
     duration: '06:26'
   },
   {      
     title: 'Oxxxymiron — Агент',
-    src: '../momentum_extantion/music/Oxxxymiron_-_Agent.mp3',
+    src: '../mmomentum_for_chrome/music/Oxxxymiron_-_Agent.mp3',
     duration: '03:35'
   },  
   {      
     title: 'Noize MC — Come $ome All',
-    src: '../momentum_extantion/music/Noize MC — Come $ome All (Тоталитарный трэпъ).mp3',
+    src: '../mmomentum_for_chrome/music/Noize MC — Come $ome All (Тоталитарный трэпъ).mp3',
     duration: '04:03'
   },
   {      
     title: 'Noize MC — Песня предателя',
-    src: '../momentum_extantion/music/Noize_MC_-_Pesnya_predatelya.mp3',
+    src: '../mmomentum_for_chrome/music/Noize_MC_-_Pesnya_predatelya.mp3',
     duration: '03:57'
   },
   {
     title: 'Дайте танк (!) — Крепость',
-    src: '../momentum_extantion/music/Дайте танк (!) - Крепость.mp3',
+    src: '../mmomentum_for_chrome/music/Дайте танк (!) - Крепость.mp3',
     duration: '03:17'
   }
 ]
